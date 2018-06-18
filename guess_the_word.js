6a83e16623b1011f1460ca40721b5a7fae990a2f
Anastasiya Lazutkina <laz-nast@mail.ru>
2018-06-18 21:54:50 +0700

    Main

--- guess_the_word.js
+++ guess_the_word.js
@@ -1,14 +1,22 @@
+// Setup
+var stdin = process.stdin;
+stdin.resume();
+stdin.setEncoding( 'utf8' );
+
+
+// State
 // word - state of game
 var word = null;
 // array of integer
 var arrayForindex = [];
 
+// Setters
 // start of game
-function init (startWord) {
+function init(startWord) {
     word = startWord;
 }
- // called on symbol
-function onSymbol (symbol) {
+// called on symbol
+function onSymbol(symbol) {
 	var pos = word.indexOf(symbol);
 	
 	while (pos !== -1) {
@@ -17,7 +25,7 @@ function onSymbol (symbol) {
 	}
 	return arrayForindex;
 }
-
+// 
 function printWord() {
 	for (i=0; i < word.length; i++) {
 		if (arrayForindex.includes(i)) {
@@ -28,3 +36,22 @@ function printWord() {
 	}
 	
 }
+
+// Getters
+function wordUnravelled() {
+	return word.length <= arrayForindex.length
+}
+
+// Main(State, Setters, Getters)
+function run(startWord) {
+	init(startWord);
+	process.stdin.setRawMode(true);
+	process.stdin.on('data', function(symbol) {
+		onSymbol(symbol);
+		console.log(symbol);
+		printWord();
+		console.log();
+		if (wordUnravelled()) process.exit();	
+	});
+}
+run("кукушка");
