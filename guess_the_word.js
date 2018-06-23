53e686cb2e03453e0d58cfb22f872d5cc7b47abf
Anastasiya Lazutkina <laz-nast@mail.ru>
2018-06-23 18:03:08 +0700

    Scoring.start

--- guess_the_word.js
+++ guess_the_word.js
@@ -9,6 +9,7 @@ stdin.setEncoding( 'utf8' );
 var word = null;
 // array of integer
 var arrayForindex = [];
+var sum = 0;
 
 // Setters
 // start of game
@@ -36,6 +37,22 @@ function printWord() {
 	}
 	
 }
+// 
+function scoring(symbol) {
+	var letters = {"а": 1,"б": 3,"в": 1,"г": 3,"д": 2,
+                   "е": 1,"ж": 5,"з": 5,"и": 1,"й": 4,
+				   "к": 2,"л": 2,"м": 2,"н": 1,"о": 1,
+				   "п": 2,"р": 1,"с": 1,"т": 1,"у": 2,
+				   "ф": 10,"х": 5,"ц": 5,"ч": 5,"ш": 8,
+				   "щ": 10,"ъ": 10,"ы": 4,"ь": 3,"э": 8,
+				   "ю": 8,"я": 3
+			   }
+	    if (word.indexOf(symbol) !== -1) {
+			sum = letters[symbol] + sum;
+		}
+		return sum;
+		
+}
 
 // Getters
 function wordUnravelled() {
@@ -48,7 +65,7 @@ function run(startWord) {
 	process.stdin.setRawMode(true);
 	process.stdin.on('data', function(symbol) {
 		onSymbol(symbol);
-		console.log(symbol);
+		console.log(symbol, scoring(symbol));
 		printWord();
 		console.log();
 		if (wordUnravelled()) process.exit();	
